dc1d94cdbd398604073780f72e1e461235f28e3c
shuo <shuoc1218@gmail.com>
2021-02-22 23:14:47 +0100

    mission complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
+import SignUp from './components/pages/SignUp'
+
 function App() {
   return (
     <>
     <Router>
       <Navbar/>
       <Switch>
-        <Router path='/' exact component={Home}/>
+        <Route path='/' exact component={Home}/>
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/signup' component={SignUp}/>
       </Switch>
     </Router>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../App.css'
+import Cards from '../Cards'
 import HeroSection from '../HeroSection'
-
+import Footer from '../Footer'
 function Home(){
-    console.log("hel")
     return(
         <>
             <HeroSection/>
+            <Cards/>
+            <Footer/>
         </>
     )
 }
